3a619619d7cad539b241ef4abb14a768e779677a
Bretzy11 <jerrytjhin155@gmail.com>
2022-04-05 08:32:23 +0700

    menambahkan perubahan v_footer,v_header

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
- <meta charset=”utf-8”>
+ <meta charset="utf-8">
  <title>Web Prog II | Merancang Template sederhana dengan 
 codeigniter</title>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() 
 ?>assets/css/stylebuku.css">
 </head>
 <body>
- <div id=”wrapper”>
+ <div id="wrapper">
  <header>
  <hgroup>
  <h1>RentalBuku.net</h1>
@@ -22,5 +22,5 @@ CodeIgniter</h3>
 base_url().'index.php/web/about' ?>">About</a></li>
  </ul>
  </nav>
- <div class=”clear”></div>
+ <div class="clear"></div>
  </header>
